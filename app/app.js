94e6236db9ca9f77b7b9c963a86218e03f318ebe
dell <dell@DESKTOP-4P8U8AR>
2024-01-24 23:50:38 +0100

    USER AUTHENTICATION : User Data Model , Connect To MongoDB Database , MongoDb Connection Sting and Environment Variable

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+import dbConnect from '../config/dbConnect.js';
+
+//dbConnect:
+dbConnect();
+const app = express();
+
+
+export default app;  
