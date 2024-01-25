f8c987b36826b7a461fa01ba56d5270dc06d3ca1
dell <dell@DESKTOP-4P8U8AR>
2024-01-25 21:32:19 +0100

    hash password of User

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
-const dbConnect =  async () => {
-    try 
-    {
-        const connected = mongoose.connect(process.env.MONGO_URL);
-        mongoose.set("strictQuery",true);
-        console.log(`Mongodb connected ${connected.connection.host} `);
-    }
-
-    catch(error){
-        console.log(`Error : ${Error.message}`);
+const dbConnect = async () => {
+    try {
+        const connected = await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        mongoose.set("strictQuery", true);
+        console.log(`MongoDB connected on ${connected.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
 
 export default dbConnect;
+
 
 //RBgMwMWlAQeLvFPc
 
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,37 @@
-import User from "../model/User";
+// usersController.js
+import User from "../model/User.js";
+import bcrypt from "bcryptjs";
 
-// @desc : Register user
-// @route : POST /api/v1/users/register
-// @access Private/Admin
+// @desc: Register user
+// @route: POST /api/v1/users/register
+// @access: Private/Admin
 
 export const registerUserController = async (req, res) => {
-    res.json({
-        msg: "User register controller",
+    // res.json({
+    //     msg: "User register controller",
+    // });
+
+    const { fullname, email, password } = req.body;
+    // **** Check if User Exists ****
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        // throw
+        res.json({
+            msg: "User already exists",
+        });
+    }
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password,salt);
+    // Create the user
+    const user = await User.create({
+        fullname,
+        email,
+        password,
+    });
+    res.status(201).json({
+        status: "success",
+        message: "User Registered Successfully",
+        data: user,
     });
 };
